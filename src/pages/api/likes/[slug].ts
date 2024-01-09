ce1fb0d930c4fc648e0f393801d413dba7456873
rcmtcristian <rcmtcristian@gmail.com>
2024-01-08 23:40:06 -0500

    🐛 Fix: likes connection ot the db R2

--- src/pages/api/likes/[slug].ts
+++ src/pages/api/likes/[slug].ts
@@ -36,6 +36,46 @@ export async function GET(context: APIContext) {
   });
 }
 
+// export async function POST(context: APIContext) {
+//   const { slug } = context.params;
+
+//   if (!slug) {
+//     return new Response('Bad Request', { status: 400 });
+//   }
+
+//   const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+
+//   if (!entry) {
+//     // Create new entry
+//     const inserted = db
+//       .insert(likes)
+//       .values({ slug, likes: 1 })
+//       .returning()
+//       .get();
+
+//     return new Response(JSON.stringify(inserted), {
+//       status: 200,
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+//   }
+
+//   const updated = db
+//     .update(likes)
+//     .set({ likes: (entry.likes ?? 0) + 1 })
+//     .where(eq(likes.slug, slug))
+//     .returning()
+//     .get();
+
+//   return new Response(JSON.stringify(updated), {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+// }
+
 export async function POST(context: APIContext) {
   const { slug } = context.params;
 
@@ -43,35 +83,40 @@ export async function POST(context: APIContext) {
     return new Response('Bad Request', { status: 400 });
   }
 
-  const entry = db.select().from(likes).where(eq(likes.slug, slug)).get();
+  try {
+    const entry = await db.select().from(likes).where(eq(likes.slug, slug)).get();
 
-  if (!entry) {
-    // Create new entry
-    const inserted = db
-      .insert(likes)
-      .values({ slug, likes: 1 })
+    if (!entry) {
+      // Create new entry
+      const inserted = await db
+        .insert(likes)
+        .values({ slug, likes: 1 })
+        .returning()
+        .get();
+
+      return new Response(JSON.stringify(inserted), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const updated = await db
+      .update(likes)
+      .set({ likes: (entry.likes ?? 0) + 1 })
+      .where(eq(likes.slug, slug))
       .returning()
       .get();
 
-    return new Response(JSON.stringify(inserted), {
+    return new Response(JSON.stringify(updated), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
       },
     });
+  } catch (error) {
+    console.error('Error:', error);
+    return new Response('Internal Server Error', { status: 500 });
   }
-
-  const updated = db
-    .update(likes)
-    .set({ likes: (entry.likes ?? 0) + 1 })
-    .where(eq(likes.slug, slug))
-    .returning()
-    .get();
-
-  return new Response(JSON.stringify(updated), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
 }
